21e67b5549f91f790e823a7cc252f211775d5685
SamGoode <samgoode1523@gmail.com>
2024-02-23 00:39:17 +1100

    added logs feature
    String utilities test now logs to logs.txt

--- String Class/String Class/String Class.cpp
+++ String Class/String Class/String Class.cpp
@@ -1,37 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <fstream>
 #include "String.h"
 
 int main() {
-    //String demo;
-    //String demo2 = "Hell";
-    //String demo3 = demo2;
-    //std::cout << demo3.Length() << std::endl; //outputs 4
-    //std::cout << demo3.CharacterAt(1) << std::endl; //outputs 'e'
-    //std::cout << (demo3.EqualTo("Hell") && demo3.EqualTo(demo2)) << std::endl; //outputs 1 (true)
-    //demo3.Append(" ooh").WriteToConsole(); //outputs 'Hell ooh'
-    //demo3.Prepend("Whoops").WriteToConsole(); //outputs 'Whoopshell ooh'
-    //std::cout << demo3.CStr() << std::endl; //also outputs 'Whoopshell ooh'
-    //demo3.ToUpper().WriteToConsole().ToLower().WriteToConsole(); //outputs 'WHOOPSHELL OH' and then outputs 'whoopshell ooh'
-    //std::cout << demo3.Find("oo") << std::endl; //outputs 2
-    //std::cout << demo3.Find(3, "oo") << std::endl; //outputs 11
-    //demo3.Replace("oo", "pizza").WriteToConsole(); //outputs 'whpizzapshell pizzah'
-    //std::cout << (demo3 == "whpizzapshell pizzah") << std::endl; //outputs 1 (true)
-    //std::cout << (demo3 != "whpizzapshell pizzah") << std::endl; //outputs 0 (false)
-    //demo = "Hello, Steve!";
-    //std::cout << demo[5] << std::endl; //outputs ','
-    //std::cout << (demo < "gandhi") << std::endl; //outputs 0 (false)
-    //std::cout << (demo < "igandhi") << std::endl; //outputs 1 (true)
-    //demo.Replace(", Steve", "").WriteToConsole(); //outputs 'Hello!'
-    //(demo + " " + demo3).WriteToConsole(); //outputs 'Hello! whpizzapshell pizzah'
-    //demo += " I am world.";
-    //demo.WriteToConsole(); //outputs 'Hello! I am world."
-    //String input;
-    //input.ReadFromConsole(); //takes user input from console
-    //std::cout << (input == "Bob") << std::endl; //returns 1 (true) if user input was 'Bob' otherwise returns false
-    //input.Prepend("Hello ").WriteToConsole(); //outputs 'Hello {input}'
-    //demo3.ReadFromConsole(); //waits for input
-
     String test = "Hello, this is a test.";
 
     struct check {
@@ -52,20 +24,35 @@ int main() {
         {test.Replace("IS", "bob").EqualTo("ABCHELLO, THbob bob A TEST.123"), "Replace"}
     };
 
-    std::time_t result = std::time(nullptr);
-    std::tm tm = *std::localtime(&result);
-    std::cout << "Date: " << tm.tm_mday << "/" << tm.tm_mon << "/" << tm.tm_year + 1900;
-    std::cout << " Time: " << tm.tm_hour << ":" << tm.tm_min << ":" << tm.tm_sec;
-    std::cout << std::endl;
+    std::ofstream logs;
+    logs.open("logs.txt", std::ios::app);
 
+    time_t result = time(nullptr);
+    tm tm = *localtime(&result);
+    logs << "Date: " << tm.tm_mday << "/" << tm.tm_mon << "/" << tm.tm_year + 1900;
+    logs << " Time: " << tm.tm_hour << ":" << tm.tm_min << ":" << tm.tm_sec;
+    logs << std::endl;
+
+    int successes = 0;
     for (int i = 0; i < sizeof(checks) / sizeof(check); i++) {    
-        std::cout << "Test " << i << " " << checks[i].text << " method ";
+        logs << "Test " << i + 1 << " " << checks[i].text << " method ";
 
         if (checks[i].condition) {
-            std::cout << "successful" << std::endl;
+            logs << "successful" << std::endl;
+            successes++;
         }
         else {
-            std::cout << "failed" << std::endl;
+            logs << "failed" << std::endl;
         }
     }
+    
+    logs << successes << "/" << sizeof(checks) / sizeof(check) << " tests successful";
+
+    for (int i = 0; i < 2; i++) {
+        logs << std::endl;
+    }
+
+    logs.close();
+
+    return 0;
 }
