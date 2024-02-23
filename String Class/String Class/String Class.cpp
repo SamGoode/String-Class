37c0b18022e536394cc6151f89d51ec0649b821f
SamGoode <samgoode1523@gmail.com>
2024-02-23 15:28:24 +1100

    Added additional test string
    test now also tests an empty string

--- String Class/String Class/String Class.cpp
+++ String Class/String Class/String Class.cpp
@@ -4,24 +4,25 @@
 #include "String.h"
 
 int main() {
-    String test = "Hello, this is a test.";
-
     struct check {
         bool condition;
         const char* text;
     };
     
-    check checks[] = { 
-        {test.Length() == 22, "Length"},
-        {test.CharacterAt(5) == ',', "CharacterAt"},
-        {test.EqualTo("Hello, this is a test."), "EqualTo"},
-        {test.Append("123").EqualTo("Hello, this is a test.123"), "Append"},
-        {test.Prepend("abc").EqualTo("abcHello, this is a test.123"), "Prepend"},
-        {test.CStr() == &test.CharacterAt(0), "CStr"},
-        {test.ToLower().EqualTo("abchello, this is a test.123"), "ToLower"},
-        {test.ToUpper().EqualTo("ABCHELLO, THIS IS A TEST.123"), "ToUpper"},
-        {test.Find("IS") == 12 && test.Find(13, "IS") == 15, "Find"},
-        {test.Replace("IS", "bob").EqualTo("ABCHELLO, THbob bob A TEST.123"), "Replace"}
+    String test = "Hello, this is a test.";
+    String test2 = "";
+
+    check checks[] = {
+        {test.Length() == 22 && test2.Length() == 0, "Length"},
+        {test.CharacterAt(5) == ',' && test2.CharacterAt(0) == NULL, "CharacterAt"},
+        {test.EqualTo("Hello, this is a test.") && test2.EqualTo(""), "EqualTo"},
+        {test.Append("123").EqualTo("Hello, this is a test.123") && test2.Append("123").EqualTo("123"), "Append"},
+        {test.Prepend("abc").EqualTo("abcHello, this is a test.123") && test2.Prepend("abc").EqualTo("abc123"), "Prepend"},
+        {test.CStr() == &test.CharacterAt(0) && test2.CStr() == &test2.CharacterAt(0), "CStr"},
+        {test.ToLower().EqualTo("abchello, this is a test.123") && test2.ToLower().EqualTo("abc123"), "ToLower"},
+        {test.ToUpper().EqualTo("ABCHELLO, THIS IS A TEST.123") && test2.ToUpper().EqualTo("ABC123"), "ToUpper"},
+        {test.Find("IS") == 12 && test.Find(13, "IS") == 15 && test2.Find("") == -1, "Find"},
+        {test.Replace("IS", "bob").EqualTo("ABCHELLO, THbob bob A TEST.123") && test2.Replace("ABC123", "").EqualTo(""), "Replace"}
     };
 
     std::ofstream logs;
@@ -34,7 +35,7 @@ int main() {
     logs << std::endl;
 
     int successes = 0;
-    for (int i = 0; i < sizeof(checks) / sizeof(check); i++) {    
+    for (int i = 0; i < sizeof(checks) / sizeof(check); i++) {
         logs << "Test " << i + 1 << " " << checks[i].text << " method ";
 
         if (checks[i].condition) {
@@ -45,7 +46,7 @@ int main() {
             logs << "failed" << std::endl;
         }
     }
-    
+
     logs << successes << "/" << sizeof(checks) / sizeof(check) << " tests successful";
 
     for (int i = 0; i < 2; i++) {
