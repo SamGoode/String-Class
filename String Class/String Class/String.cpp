3b350ecdc2f7665a033151a9edf11b8bd4c1073b
Jevan Soh <s240150@students.aie.edu.au>
2024-02-28 15:01:38 +1100

    fixed ReadFromConsole bug
    ReadFromConsole wasn't properly clearing input stream buffer.

--- String Class/String Class/String.cpp
+++ String Class/String Class/String.cpp
@@ -226,7 +226,8 @@ String& String::ReadFromConsole() {
     length = bufferSize - 1;
     delete[] dataPtr;
     dataPtr = new char[length + 1];
-    for (int i = 0; i < length; i++) {
+    //need to loop to buffersize to clear null value from buffer.
+    for (int i = 0; i < bufferSize; i++) {
         std::cin.get(*(dataPtr + i));
     }
     *(dataPtr + length) = 0;
